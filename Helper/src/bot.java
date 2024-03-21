f7e120cead4b8dd8d4e5879a12090c238f32e255
Mitilan02 <mitilan02@gmail.com>
2024-03-21 11:56:18 +0530

    commit

--- Helper/src/bot.java
+++ Helper/src/bot.java
@@ -1,74 +1,179 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
-import weka.classifiers.Classifier;
+
 import weka.classifiers.trees.J48;
-import weka.core.DenseInstance;
-import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 
 public class bot {
 
-    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Replace this array with your actual symptoms list
+        String[] symptoms = {
+                "itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing", "shivering",
+                "chills", "joint_pain", "stomach_pain", "acidity", "ulcers_on_tongue", "muscle_wasting",
+                "vomiting", "burning_micturition", "spotting_urination", "fatigue", "weight_gain", "anxiety",
+                "cold_hands_and_feets", "mood_swings", "weight_loss", "restlessness", "lethargy", "patches_in_throat",
+                "irregular_sugar_level", "cough", "high_fever", "sunken_eyes", "breathlessness", "sweating", "dehydration",
+                "indigestion", "headache", "yellowish_skin", "dark_urine", "nausea", "loss_of_appetite", "pain_behind_the_eyes",
+                "back_pain", "constipation", "abdominal_pain", "diarrhoea", "mild_fever", "yellow_urine", "yellowing_of_eyes",
+                "acute_liver_failure", "fluid_overload", "swelling_of_stomach", "swelled_lymph_nodes", "malaise",
+                "blurred_and_distorted_vision", "phlegm", "throat_irritation", "redness_of_eyes", "sinus_pressure",
+                "runny_nose", "congestion", "chest_pain", "weakness_in_limbs", "fast_heart_rate", "pain_during_bowel_movements",
+                "pain_in_anal_region", "bloody_stool", "irritation_in_anus", "neck_pain", "dizziness", "cramps", "bruising",
+                "obesity", "swollen_legs", "swollen_blood_vessels", "puffy_face_and_eyes", "enlarged_thyroid", "brittle_nails",
+                "swollen_extremities", "excessive_hunger", "extra_marital_contacts", "drying_and_tingling_lips", "slurred_speech",
+                "knee_pain", "hip_joint_pain", "muscle_weakness", "stiff_neck", "swelling_joints", "movement_stiffness",
+                "spinning_movements", "loss_of_balance", "unsteadiness", "weakness_of_one_body_side", "loss_of_smell",
+                "bladder_discomfort", "foul_smell_of_urine", "continuous_feel_of_urine", "passage_of_gases",
+                "internal_itching", "toxic_look_(typhos)", "depression", "irritability", "muscle_pain", "altered_sensorium",
+                "red_spots_over_body", "belly_pain", "abnormal_menstruation", "dischromic_patches", "watering_from_eyes",
+                "increased_appetite", "polyuria", "family_history", "mucoid_sputum", "rusty_sputum", "lack_of_concentration",
+                "visual_disturbances", "receiving_blood_transfusion", "receiving_unsterile_injections", "coma", "stomach_bleeding",
+                "distention_of_abdomen", "history_of_alcohol_consumption", "blood_in_sputum", "prominent_veins_on_calf",
+                "palpitations", "painful_walking", "pus_filled_pimples", "blackheads", "scurring", "skin_peeling",
+                "silver_like_dusting", "small_dents_in_nails", "inflammatory_nails", "blister", "red_sore_around_nose",
+                "yellow_crust_ooze"
+        };
 
-    public static void main(String[] args) throws Exception {
-        // Convert CSV to ARFF if ARFF doesn't exist
-        File arffFile = new File("Data/trainingSet.arff");
-        if (!arffFile.exists()) {
-            convertCSVtoARFF("Data/trainingSet.csv", "Data/trainingSet.arff");
+        // Replace "tree" with the actual decision tree object you created
+        J48 tree = createDecisionTree();
+
+        // Load training data
+        Instances data = loadData();
+
+        // Make a prediction
+        String predictedDisease = predictDisease(tree, symptoms, data);
+
+        System.out.println("Predicted Disease: " + predictedDisease);
+    }
+
+    private static J48 createDecisionTree() {
+        try {
+            // Load training data
+            CSVLoader loader = new CSVLoader();
+            loader.setSource(new File("Data/Dataset.csv"));
+            Instances data = loader.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1);
+
+            // Build a decision tree
+            J48 tree = new J48();
+            tree.buildClassifier(data);
+
+            // Return the decision tree
+            return tree;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
+    }
 
-        // Load the training dataset
-        Instances trainingData = loadTrainingData("Data/trainingSet.arff");
+    private static Instances loadData() {
+        try {
+            CSVLoader loader = new CSVLoader();
+            loader.setSource(new File("Data/Dataset.csv"));
+            Instances data = loader.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1);
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-        // Train a decision tree classifier
-        Classifier classifier = new J48();
-        classifier.buildClassifier(trainingData);
+    private static String predictDisease(J48 tree, String[] symptoms, Instances data) {
+        try {
+            // Manually traverse the decision tree based on user input
+            Scanner scanner = new Scanner(System.in);
 
-        // Chatbot interaction loop
-        while (true) {
-            System.out.println("What symptom do you have? (Type 'exit' to quit)");
-            String symptom = scanner.nextLine();
-            if (symptom.equalsIgnoreCase("exit")) {
-                break;
+            int node = 0;
+            while (true) {
+                System.out.println("Current Node: " + node);
+                System.out.println("Question: " + getQuestion(tree, node));
+                System.out.print("Enter 1 for Yes, 0 for No: ");
+
+                int answer = scanner.nextInt();
+                // Extract the next node based on user input
+                node = getNextNode(tree, node, answer, data);
+
+                // If it's a leaf node, break the loop
+                if (node == -1) {
+                    break;
+                }
             }
-            
-            // Create an instance for prediction
-            Instance instance = new DenseInstance(1.0, new double[trainingData.numAttributes()]);
-            instance.setDataset(trainingData);
-            instance.setValue(trainingData.attribute("Symptom"), symptom);
-            
-            // Predict disease based on user's response
-            double prediction = classifier.classifyInstance(instance);
-            String predictedDisease = trainingData.classAttribute().value((int) prediction);
-            System.out.println("Predicted disease: " + predictedDisease);
+
+            // Get the predicted disease label from the leaf node
+            double[] distribution = tree.distributionForInstance(data.instance(0));
+            int predictedClassIndex = (int) tree.classifyInstance(data.instance(0));
+            String predictedDisease = data.classAttribute().value(predictedClassIndex);
+
+            return predictedDisease;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error predicting disease.";
         }
     }
 
-    private static Instances loadTrainingData(String filename) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        Instances data = new Instances(reader);
-        reader.close();
-        // Set the class attribute
-        data.setClassIndex(data.numAttributes() - 1);
-        return data;
+
+    private static String getQuestion(J48 tree, int node) {
+            try {
+                // Extract the question from the decision tree
+                if (tree == null || tree.graph() == null) {
+                    return "Unknown Question";
+                }
+
+                String[] lines = tree.graph().split("\n");
+                for (String line : lines) {
+                    if (line.startsWith("N" + node)) {
+                        int startIndex = line.indexOf("[") + 1;
+                        int endIndex = line.indexOf("]");
+                        return line.substring(startIndex, endIndex).trim();
+                    }
+                }
+                return "Unknown Question";
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Error getting question.";
+            }
+        }
+
+    private static int getNextNode(J48 tree, int currentNode, int answer, Instances data) {
+        try {
+            // Extract the next node based on user input
+            if (tree == null || tree.graph() == null) {
+                return -1;
+            }
+
+            String[] lines = tree.graph().split("\n");
+            for (String line : lines) {
+                if (line.startsWith("N" + currentNode)) {
+                    String[] parts = line.split("->");
+                    if (parts.length > 1) {
+                        String successor = parts[1].trim().split(" ")[0];
+                        if (successor.startsWith("N")) {
+                            // If it's a non-leaf node, extract the node number
+                            return Integer.parseInt(successor.substring(1));
+                        } else if (successor.startsWith("leaf")) {
+                            // If it's a leaf node, return -1 to break the loop
+                            return -1;
+                        } else {
+                            // Handle special case for nodes like '(vertigo) Paroymsal  Positional Vertigo (10.0/5.0)'
+                            successor = successor.split(" ")[0];
+                            if (successor.startsWith("(vertigo)")) {
+                                successor = successor.substring(successor.indexOf("(") + 1, successor.indexOf(")"));
+                                return Integer.parseInt(successor);
+                            }
+                        }
+                    }
+                }
+            }
+            return -1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 
-    private static void convertCSVtoARFF(String csvFile, String arffFile) throws IOException {
-        // Load CSV file
-        CSVLoader loader = new CSVLoader();
-        loader.setSource(new File(csvFile));
-        Instances data = loader.getDataSet();
 
-        // Save as ARFF
-        ArffSaver saver = new ArffSaver();
-        saver.setInstances(data);
-        saver.setFile(new File(arffFile));
-        saver.writeBatch();
+        
     }
-}
